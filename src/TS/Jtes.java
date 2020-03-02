d3e25d00cd121573de9bd6e01f4fcae00cf1586f
samah <samah@DESKTOP-KFJ942F>
2020-03-02 17:44:52 +0200

    build2

--- src/TS/Jtes.java
+++ src/TS/Jtes.java
@@ -2,27 +2,53 @@ package TS;
 
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
+//import org.junit.BeforeClass;
 import org.junit.Test;
 import SR.Cal;
 
 public class Jtes {
-	/*@BeforeClass
-	void setup() throws Exception {
+	
+	static Cal Obj1;
+	
+	@Before
+	public void  setup() throws Exception {
+	 Obj1 = new Cal();
+	}
+	
+	@Test
+	public void tearDown() throws Exception{
 		
 	}
 	
 	@Test
-	void tearDown() throws Exception
-	{
-		
-	}*/
-	@Test
-	public void test() {
-		Cal ob=new Cal();
-		int act=ob.add(4,2);
+	public void test1() {
+		int act=Obj1.add(4,2);
 		assertTrue(6==act);
-
+	}
+	
+	@Test
+	public void test2() {
+		int act=Obj1.add(-4,2);
+		assertTrue(-2==act);
+	}
+	
+	@Test
+	public void test3() {
+		int act=Obj1.add(4,-2);
+		assertTrue(2==act);
+	}
+	
+	@Test
+	public void test4() {
+		int act=Obj1.add(-4,-2);
+		assertTrue(-6==act);
+	}
+	
+	@Test
+	public void test5() {
+		int act=Obj1.add(7,-2);
+		assertTrue(5==act);
 	}
 
 }
